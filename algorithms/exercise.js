a7e1a5f6c41a1dcb98ca935025e56fd8497510e2
Tim Stark <timstarkk@icloud.com>
2020-05-10 21:23:16 -0500

    added recursive algorithm for reversing string

--- algorithms/exercise.js
+++ algorithms/exercise.js
@@ -1,7 +1,13 @@
 //Implement a function that reverses a string using iteration...and then recursion!
-function reverseString(str) {
 
+function reverseString(str) {
+    
+    if (str === "") {
+        return "";
+    }
+    
+    return reverseString(str.substr(1)) + str.charAt(0);
 }
 
-reverseString('yoyo mastery')
+console.log(reverseString('yoyo mastery'));
 //should return: 'yretsam oyoy'
