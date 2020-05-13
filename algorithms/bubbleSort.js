e6fbcebeeb0d645bf56d574a93b0fd3becbe2089
Tim Stark <timstarkk@icloud.com>
2020-05-13 02:43:11 -0500

    finished simple bubblesort algorithm

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -3,6 +3,14 @@ let array = [5,23,123,32,4,235,3,21,321,4,7]
 bubbleSort = array => {
 
     for (const num of array) {
-        
-    }
+        for (let i = 0; i < array.length - 1; i ++) {
+            if (array[i] > array[i+1]) {
+                [array[i], array[i+1]] = [array[i+1], array[i]];
+            }
+        }
+    };
+
+    return array;
 };
+
+console.log(bubbleSort(array));
