067d8c663aca0219db7d3fd291fa4739a95a0741
Tim Stark <timstarkk@icloud.com>
2020-05-30 11:57:11 -0500

    work on merge sort algorithm

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -1,10 +1,16 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function mergeSort (array) {
+  console.log(array);
   if (array.length === 1) {
     return array
   }
+  
   // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
 
   return merge(
     mergeSort(left),
@@ -12,8 +18,37 @@ function mergeSort (array) {
   )
 }
 
-function merge(left, right){
+function merge(L, R){
+  let nR = R.length;
+  let nL = L.length;
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  let A = [];
 
+  while (i < nL && j < nR) {
+    if( L[i] <= R[j]) {
+      A[k] = L[i];
+      i++;
+    } else {
+      A[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  // when one side is finished, add all elements from remaining side
+  // (only one of these while loops will execute)
+  while (i < nL) {
+    A[k] = L[i]; 
+    i++;
+    k++;
+  }
+  while (j < nR) {
+    A[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
 
